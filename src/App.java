d96056141373b8f2d304484daf90e04535ca4595
HappyZtao <3302523993@qq.com>
2025-01-20 12:16:56 +0800

    提交

--- src/App.java
+++ src/App.java
@@ -1,7 +1,27 @@
+import java.io.*;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
+        int flag = exceptionFunc("test.txt");
+        System.out.println(flag);
 
+    }
+
+    public static int exceptionFunc(String fileName){
+        try {
+            FileInputStream file = new FileInputStream(fileName);
+            byte x = (byte) file.read();
+        } catch(FileNotFoundException f) { // Not valid!
+            f.printStackTrace();
+            return -1;
+        } catch(IOException i) {
+            i.printStackTrace();
+            return -1;
+        }
+        return 0;
     }
 }
 
-class Vehicle{}
+
+
